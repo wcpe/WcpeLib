af8bcd2643355912d5a568f2b17302784dd2b99e
WCPE <1837019522@qq.com>
2021-05-23 19:02:00 +0800

    update#5-23>update ItemStackUtil

--- bukkit/src/main/java/top/wcpe/wcpelib/bukkit/utils/ItemStackUtil.java
+++ bukkit/src/main/java/top/wcpe/wcpelib/bukkit/utils/ItemStackUtil.java
@@ -1,8 +1,11 @@
 package top.wcpe.wcpelib.bukkit.utils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -53,32 +56,110 @@ public class ItemStackUtil {
         return item;
     }
 
-
-    public static int loreContainsNumCount(List<String> lore, String con) {
-        int i = 0;
-        for (String s : lore) {
-            if (s.equals(con)) {
-                i++;
+    @Deprecated
+    public static void clearItem(Player p, int id, int data, String name, List<String> lore, int number) {
+        final HashMap<Integer, Integer> idAmound = new HashMap<>();
+        for (int i = 0; i < p.getInventory().getMaxStackSize(); i++) {
+            ItemStack item = p.getInventory().getItem(i);
+            if (item != null) {
+                ItemMeta im = item.getItemMeta();
+                if (item.getTypeId() == id && item.getData().getData() == data && name.equals(im.getDisplayName())
+                        && equalsLore(lore, im.getLore())) {
+                    idAmound.put(i, item.getAmount());
+                }
             }
         }
-        return i;
+        for (Map.Entry<Integer, Integer> entry : idAmound.entrySet()) {
+            Integer key = entry.getKey();
+            Integer value = entry.getValue();
+            if (number == value) {
+                p.getInventory().setItem(key, null);
+                return;
+            } else if (number > value) {
+                number -= value;
+                p.getInventory().setItem(key, null);
+            } else if (number < value) {
+                p.getInventory().getItem(key).setAmount(value - number);
+                return;
+            }
+        }
+
     }
 
+    /**
+     * itemLore 中是否包含 configLore
+     *
+     * @param configLore
+     * @param itemLore
+     * @return boolean
+     */
     public static boolean itemLoreContainLore(List<String> configLore, List<String> itemLore) {
-        if (configLore.size() <= itemLore.size()) {
-            for (String s : configLore) {
-                int configLoreNum = loreContainsNumCount(configLore, s);
-                if (configLoreNum > 1) {
-                    if (loreContainsNumCount(itemLore, s) == configLoreNum) {
-                        continue;
-                    }
-                    return false;
-                }
-                if (!itemLore.contains(s)) {
-                    return false;
-                }
+        if (configLore == null || itemLore == null) return false;
+        if (configLore.size() > itemLore.size()) return false;
+        for (int i = 0; i < itemLore.size(); i++) {
+            if (!itemLore.contains(configLore.get(i))) {
+                return false;
             }
+        }
+        return true;
+
+    }
+
+    /**
+     * 对比两个lore是否相等
+     *
+     * @param lores
+     * @param lores2
+     * @return {@link boolean}
+     * @author WCPE
+     * @date 2021年4月20日 下午6:03:43
+     */
+    public static boolean equalsLore(List<String> lores, List<String> lores2) {
+        if (lores == null && lores2 == null) {
             return true;
+        }
+        if ((lores == null && lores2 != null) || (lores2 == null && lores != null)) {
+            return false;
+        }
+        if (lores.size() != lores2.size())
+            return false;
+        for (int i = 0; i < lores.size(); i++) {
+            if (!lores.get(i).equals(lores2.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * ItemStack 比对物品
+     *
+     * @param item1
+     * @param item2
+     * @return
+     */
+    public static boolean equalsItem(ItemStack item1, ItemStack item2) {
+        if (item1 == null && item2 == null) return true;
+        if (item1 == null && item2 != null || item2 == null && item1 != null) return false;
+        if (item1.getType() != item2.getType()) return false;
+        if (item1.getDurability() != item2.getDurability()) return false;
+        if (item1.getAmount() != item2.getAmount()) return false;
+        if (!item1.getData().equals(item2.getData())) return false;
+        if (!item1.hasItemMeta() && !item2.hasItemMeta()) return true;
+        if (item1.hasItemMeta() && !item2.hasItemMeta() || item2.hasItemMeta() && !item1.hasItemMeta()) return false;
+        ItemMeta item1Meta = item1.getItemMeta();
+        ItemMeta item2Meta = item2.getItemMeta();
+        String item1DisplayName = item1Meta.getDisplayName();
+        String item2DisplayName = item2Meta.getDisplayName();
+        if (item1DisplayName != null && item2DisplayName == null || item2DisplayName != null && item1DisplayName == null)
+            return false;
+        List<String> item1Lore = item1Meta.getLore();
+        List<String> item2Lore = item2Meta.getLore();
+        if (equalsLore(item1Lore, item2Lore)) {
+            if (item1DisplayName == null && item2DisplayName == null) {
+                return true;
+            }
+            if (item1DisplayName.equals(item2DisplayName)) return true;
         }
         return false;
     }
