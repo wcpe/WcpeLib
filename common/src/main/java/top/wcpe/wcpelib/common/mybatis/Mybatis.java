5d1e4b338734c4d09513dc8b1391fd390280259d
WCPE <1837019522@qq.com>
2021-05-30 19:05:00 +0800

    <feat>(Mybatis): 添加数据库名称字段

--- common/src/main/java/top/wcpe/wcpelib/common/mybatis/Mybatis.java
+++ common/src/main/java/top/wcpe/wcpelib/common/mybatis/Mybatis.java
@@ -10,11 +10,15 @@ import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 import lombok.Getter;
 
 public class Mybatis {
+    @Getter
+    private final String databaseName;
+
     public Mybatis(String url, int port, String database, String user, String password) {
         this(url, port, database, "useSSL=false&characterEncoding=UTF-8&serverTimezone=UTC", user, password);
     }
 
     public Mybatis(String url, int port, String database, String parameter, String user, String password) {
+        this.databaseName = database;
         PooledDataSource pooledDataSource = new PooledDataSource("com.mysql.jdbc.Driver",
                 "jdbc:mysql://" + url + ":" + port + "/" + database + "?" + parameter, user,
                 password);
@@ -27,6 +31,7 @@ public class Mybatis {
         Configuration configuration = new Configuration(environment);
         this.sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
     }
+
     @Getter
     private SqlSessionFactory sqlSessionFactory;
 
