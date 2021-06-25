50908e7ed0191f53e0ac1d6b08927dfc7d079ea2
WCPE <1837019522@qq.com>
2021-06-25 12:37:24 +0800

    <feat>(Nukkit): 新增 EconomyUtil

---
+++ nukkit/src/main/java/top/wcpe/wcpelib/nukkit/otherpluginapi/economyapi/EconomyUtil.java
@@ -0,0 +1,24 @@
+package top.wcpe.wcpelib.nukkit.otherpluginapi.economyapi;
+
+import cn.nukkit.Server;
+import me.onebone.economyapi.EconomyAPI;
+
+/**
+ * EconomyAPI 插件
+ *
+ * @author: WCPE 1837019522@qq.com
+ * @create: 2021-06-24 20:17
+ */
+public class EconomyUtil {
+    private static EconomyAPI economy = null;
+
+
+    public static EconomyAPI getEconomy() {
+        if (economy == null)
+            if (Server.getInstance().getPluginManager().getPlugin("EconomyAPI") != null) {
+                economy = EconomyAPI.getInstance();
+            }
+        return economy;
+    }
+
+}
