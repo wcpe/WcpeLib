fbd7405616e2e3752c602e4cffedb2e5d863c4ec
WCPE <1837019522@qq.com>
2021-09-22 22:12:16 +0800

    <feat>: 更新压缩包工具

---
+++ common/src/main/java/top/wcpe/wcpelib/common/utils/zip/ZipUtil.java
@@ -0,0 +1,76 @@
+package top.wcpe.wcpelib.common.utils.zip;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * 压缩包管理工具
+ *
+ * @author: WCPE 1837019522@qq.com
+ * @create: 2021-09-17 21:39
+ */
+public class ZipUtil {
+    public static void compressToPath(final Path sourcePath, final Path outPath, final boolean keepDirStructure) {
+        if (Files.notExists(sourcePath)) {
+            return;
+        }
+        new Thread(() -> {
+            ZipOutputStream zipOutputStream = null;
+            try {
+                compress(sourcePath.toFile(), zipOutputStream = new ZipOutputStream(Files.newOutputStream(outPath)), sourcePath.toFile().getName(), keepDirStructure);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (zipOutputStream != null) {
+                    try {
+                        zipOutputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private static void compress(File sourceFile, ZipOutputStream zos, String name, boolean keepDirStructure) throws IOException {
+        byte[] buf = new byte[2048];
+        if (sourceFile.isFile()) {
+            zos.putNextEntry(new ZipEntry(name));
+            FileInputStream in = new FileInputStream(sourceFile);
+            int len;
+            while ((len = in.read(buf)) != -1)
+                zos.write(buf, 0, len);
+            zos.closeEntry();
+            in.close();
+        } else {
+            File[] listFiles = sourceFile.listFiles();
+            if (listFiles == null || listFiles.length == 0) {
+                if (keepDirStructure) {
+                    zos.putNextEntry(new ZipEntry(name + "/"));
+                    zos.closeEntry();
+                }
+            } else {
+                byte b;
+                int i;
+                File[] arrayOfFile;
+                for (i = (arrayOfFile = listFiles).length, b = 0; b < i; ) {
+                    File file = arrayOfFile[b];
+                    if (keepDirStructure) {
+                        compress(file, zos, name + "/" + file.getName(), true);
+                    } else {
+                        compress(file, zos, file.getName(), false);
+                    }
+                    b++;
+                }
+            }
+        }
+    }
+
+}
