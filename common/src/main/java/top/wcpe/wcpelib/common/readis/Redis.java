416a06e5446a9234ca274b6812312755ea5fffcb
WCPE <1837019522@qq.com>
2021-12-08 12:53:38 +0800

    <feat>(common): Redis 更新参数

--- common/src/main/java/top/wcpe/wcpelib/common/readis/Redis.java
+++ common/src/main/java/top/wcpe/wcpelib/common/readis/Redis.java
@@ -11,31 +11,34 @@ import redis.clients.jedis.JedisPoolConfig;
  * @create: 2021-05-11 10:05
  */
 public class Redis {
+
     private JedisPool jedisPool;
 
     public Redis(String url, int port) {
         jedisPool = new JedisPool(url, port);
     }
 
-    public Redis(String url, int port, int maxTotal, int maxIdle, int minIdle, int maxWaitMillis, boolean testOnBorrow, boolean testOnReturn) {
+    public Redis(String url, int port, int timeOut, int maxTotal, int maxIdle, int minIdle, boolean jmxEnabled, boolean testOnCreate, boolean blockWhenExhausted, int maxWaitMillis, boolean testOnBorrow, boolean testOnReturn) {
+        this(url, port, timeOut, null, maxTotal, maxIdle, minIdle, jmxEnabled, testOnCreate, blockWhenExhausted, maxWaitMillis, testOnBorrow, testOnReturn);
+    }
+
+    public Redis(String url, int port, int timeOut, String password, int maxTotal, int maxIdle, int minIdle, boolean jmxEnabled, boolean testOnCreate, boolean blockWhenExhausted, int maxWaitMillis, boolean testOnBorrow, boolean testOnReturn) {
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
         jedisPoolConfig.setMaxTotal(maxTotal);
         jedisPoolConfig.setMaxIdle(maxIdle);
         jedisPoolConfig.setMinIdle(minIdle);
+        jedisPoolConfig.setJmxEnabled(jmxEnabled);
+
+        jedisPoolConfig.setTestOnCreate(testOnCreate);
+        jedisPoolConfig.setBlockWhenExhausted(blockWhenExhausted);
         jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
         jedisPoolConfig.setTestOnBorrow(testOnBorrow);
         jedisPoolConfig.setTestOnReturn(testOnReturn);
-        jedisPool = new JedisPool(jedisPoolConfig, url, port);
+        jedisPool = new JedisPool(jedisPoolConfig, url, port, timeOut, password);
     }
 
     public Jedis getResource() {
         return jedisPool.getResource();
     }
-    public  void returnBrokenResource(Jedis jedis){
-        jedisPool.returnBrokenResource(jedis);
-    }
 
-    public  void returnResource(Jedis jedis){
-        jedisPool.returnResource(jedis);
-    }
 }
