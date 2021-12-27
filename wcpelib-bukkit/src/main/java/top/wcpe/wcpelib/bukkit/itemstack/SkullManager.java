af19751aa26cefff3baa774a853d7706e43f3a22
WCPE <1837019522@qq.com>
2021-12-27 23:56:44 +0800

    <modify>: update 1.0.5-alpha-dev-1
    修改 build.gradle 和各个模块名称
    同步各个模块版本
    更新部分依赖版本
    新增 okhttp gson 依赖
    新增 .gitignore
    删除 org.json 解析库

---
+++ wcpelib-bukkit/src/main/java/top/wcpe/wcpelib/bukkit/itemstack/SkullManager.java
@@ -0,0 +1,156 @@
+package top.wcpe.wcpelib.bukkit.itemstack;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.SkullType;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+import top.wcpe.wcpelib.bukkit.WcpeLib;
+import top.wcpe.wcpelib.bukkit.utils.NetMinecraftServerUtil;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 玩家头颅
+ *
+ * @author: WCPE 1837019522@qq.com
+ * @create: 2021-05-12 10:57
+ */
+public class SkullManager {
+    public static AtomicInteger limitTime = new AtomicInteger(0);
+    private static HashMap<String, String[]> playerSkin = new HashMap<>();
+
+    static {
+        Bukkit.getScheduler().runTaskTimerAsynchronously(WcpeLib.getInstance(), () -> limitTime.set(0), 12000L, 12000L);
+        Bukkit.getScheduler().runTaskTimerAsynchronously(WcpeLib.getInstance(), () -> {
+            try {
+                for (Player p : Bukkit.getOnlinePlayers())
+                    savePlayerSkullSkin(p);
+            } catch (Exception exception) {
+            }
+        }, (20 * WcpeLib.getInstance().getConfig().getInt("skull-save-time")), (20 * WcpeLib.getInstance().getConfig().getInt("Setting.skullSaveTime")));
+    }
+
+    static void savePlayerSkullSkin(Player p) {
+        String[] playerTexture;
+        try {
+            Object entityPlayer = p.getClass().getMethod("getHandle", new Class[0]).invoke(p, new Object[0]);
+            Method getProfileMethod = entityPlayer.getClass().getMethod("getProfile", new Class[0]);
+            GameProfile gameProfile = (GameProfile) getProfileMethod.invoke(entityPlayer, new Object[0]);
+            Property property = gameProfile.getProperties().get("textures").iterator().next();
+            playerTexture = new String[]{property.getSignature(), property.getValue()};
+        } catch (Exception e) {
+            playerTexture = new String[]{"K9P4tCIENYbNpDuEuuY0shs1x7iIvwXi4jUUVsATJfwsAIZGS+9OZ5T2HB0tWBoxRvZNi73Vr+syRdvTLUWPusVXIg+2fhXmQoaNEtnQvQVGQpjdQP0TkZtYG8PbvRxE6Z75ddq+DVx/65OSNHLWIB/D+Rg4vINh4ukXNYttn9QvauDHh1aW7/IkIb1Bc0tLcQyqxZQ3mdglxJfgIerqnlA++Lt7TxaLdag4y1NhdZyd3OhklF5B0+B9zw/qP8QCzsZU7VzJIcds1+wDWKiMUO7+60OSrIwgE9FPamxOQDFoDvz5BOULQEeNx7iFMB+eBYsapCXpZx0zf1bduppBUbbVC9wVhto/J4tc0iNyUq06/esHUUB5MHzdJ0Y6IZJAD/xIw15OLCUH2ntvs8V9/cy5/n8u3JqPUM2zhUGeQ2p9FubUGk4Q928L56l3omRpKV+5QYTrvF+AxFkuj2hcfGQG3VE2iYZO6omXe7nRPpbJlHkMKhE8Xvd1HP4PKpgivSkHBoZ92QEUAmRzZydJkp8CNomQrZJf+MtPiNsl/Q5RQM+8CQThg3+4uWptUfP5dDFWOgTnMdA0nIODyrjpp+bvIJnsohraIKJ7ZDnj4tIp4ObTNKDFC/8j8JHz4VCrtr45mbnzvB2DcK8EIB3JYT7ElJTHnc5BKMyLy5SKzuw=", "eyJ0aW1lc3RhbXAiOjE1MjkyNTg0MTE4NDksInByb2ZpbGVJZCI6Ijg2NjdiYTcxYjg1YTQwMDRhZjU0NDU3YTk3MzRlZWQ3IiwicHJvZmlsZU5hbWUiOiJTdGV2ZSIsInNpZ25hdHVyZVJlcXVpcmVkIjp0cnVlLCJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZGMxYzc3Y2U4ZTU0OTI1YWI1ODEyNTQ0NmVjNTNiMGNkZDNkMGNhM2RiMjczZWI5MDhkNTQ4Mjc4N2VmNDAxNiJ9LCJDQVBFIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjc2N2Q0ODMyNWVhNTMyNDU2MTQwNmI4YzgyYWJiZDRlMjc1NWYxMTE1M2NkODVhYjA1NDVjYzIifX19"};
+            if (limitTime.get() >= 400) {
+                return;
+
+            }
+            String value = sendGetMojangApi(p.getName());
+            limitTime.addAndGet(1);
+            if (value == null) {
+                return;
+            }
+            playerTexture[1] = value;
+        }
+        playerSkin.put(p.getName(), playerTexture);
+    }
+
+    public static String sendGetMojangApi(String playerName) {
+        try {
+            String result = sendGet("https://api.mojang.com/users/profiles/minecraft/" + playerName);
+            JsonParser parser = new JsonParser();
+            JsonObject jsonObject = parser.parse(result).getAsJsonObject();
+            String uid = jsonObject.getAsJsonPrimitive("id").getAsString();
+            String signature = sendGet("https://sessionserver.mojang.com/session/minecraft/profile/" + uid);
+            jsonObject = parser.parse(signature).getAsJsonObject();
+            jsonObject = jsonObject.getAsJsonArray("properties").get(0).getAsJsonObject();
+            String value = jsonObject.getAsJsonPrimitive("value").getAsString();
+            String decoded = new String(Base64.getDecoder().decode(value));
+            jsonObject = parser.parse(decoded).getAsJsonObject();
+            String skinURL = jsonObject.getAsJsonObject("textures").getAsJsonObject("SKIN").getAsJsonPrimitive("url").getAsString();
+            byte[] skinByte = ("{\"textures\":{\"SKIN\":{\"url\":\"" + skinURL + "\"}}}").getBytes();
+            return new String(Base64.getEncoder().encode(skinByte));
+        } catch (Exception exception) {
+            return null;
+        }
+    }
+
+    public static String sendGet(String urlStr) {
+        BufferedReader in = null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            URL url = new URL(urlStr);
+            in = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
+            String str;
+            while ((str = in.readLine()) != null)
+                sb.append(str);
+        } catch (Exception exception) {
+            try {
+                if (in != null)
+                    in.close();
+            } catch (IOException iOException) {
+            }
+        } finally {
+            try {
+                if (in != null)
+                    in.close();
+            } catch (IOException iOException) {
+            }
+        }
+        return sb.toString();
+    }
+
+
+    public static ItemStack getSkullItemStack() {
+        ItemStack skull;
+        if (NetMinecraftServerUtil.getServerVersionNum() > 1122) {
+            skull = new ItemStack(Material.valueOf("PLAYER_HEAD"));
+        } else {
+            skull = new ItemStack(Material.getMaterial("SKULL_ITEM"), 1, (short) SkullType.PLAYER.ordinal());
+        }
+        return skull;
+    }
+
+    public static ItemStack getSkullItemStack(String playerName) {
+        String[] pSk = playerSkin.get(playerName);
+        if (pSk == null) {
+            return getSkullItemStack();
+        }
+        String signature = pSk[0];
+        String value = pSk[1];
+        ItemStack is = getSkullItemStack();
+        SkullMeta sm = (SkullMeta) is.getItemMeta();
+        GameProfile gameProfile = new GameProfile(UUID.randomUUID(), null);
+        gameProfile.getProperties().put("textures", new Property("textures", value, signature));
+        try {
+            Field profileField = sm.getClass().getDeclaredField("profile");
+            profileField.setAccessible(true);
+            profileField.set(sm, gameProfile);
+        } catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e1) {
+            e1.printStackTrace();
+        }
+        is.setItemMeta((ItemMeta) sm);
+        return is;
+
+    }
+
+}
---
+++ wcpelib-nukkit/src/main/kotlin/top/wcpe/wcpelib/nukkit/chat/ChatAcceptParameterManager.kt
@@ -0,0 +1,86 @@
+package top.wcpe.wcpelib.nukkit.chat
+
+import cn.nukkit.Player
+import cn.nukkit.Server
+import cn.nukkit.event.EventHandler
+import cn.nukkit.event.EventPriority
+import cn.nukkit.event.Listener
+import cn.nukkit.event.player.PlayerChatEvent
+import lombok.Data
+import top.wcpe.wcpelib.nukkit.WcpeLib
+import top.wcpe.wcpelib.nukkit.chat.entity.ChatAcceptParameterTask
+
+/**
+ * 由 WCPE 在 2021/12/18 19:32 创建
+ *
+ * Created by WCPE on 2021/12/18 19:32
+ *
+ * Github: https://github.com/wcpe
+ *
+ * QQ: 1837019522
+ *
+ * @author WCPE
+ */
+object ChatAcceptParameterManager {
+
+    @JvmStatic
+    val playerTask: MutableMap<String, ChatAcceptParameterTask> = mutableMapOf()
+
+
+    init {
+        Server.getInstance().pluginManager.registerEvents(object : Listener {
+            @EventHandler(priority = EventPriority.HIGH)
+            fun playerChatEvent(e: PlayerChatEvent) {
+                playerTask[e.player.name]?.let {
+                    if (it.timeStamp < System.currentTimeMillis()) {
+                        playerTask.remove(e.player.name)
+                        return
+                    }
+
+                    e.isCancelled = true
+
+                    if (it.cancelJudgeTask(e.player, e.message)) {
+                        it.cancelSuccessTask(e.player, e.message)
+                        playerTask.remove(e.player.name)
+                        return
+                    }
+                    if (it.judge(e.player, e.message)) {
+                        it.judgeTrueTask(e.player, e.message)
+                        playerTask.remove(e.player.name)
+                        return
+                    }
+                    it.judgeFalseTask(e.player, e.message)
+                }
+            }
+        }, WcpeLib.getInstance())
+
+    }
+
+    fun putChatAcceptParameterTask(
+        player: Player,
+        inputTime: Long,
+        cancelJudgeTask: (player: Player, message: String) -> Boolean,
+        cancelSuccessTask: (player: Player, message: String) -> Unit,
+        judge: (player: Player, message: String) -> Boolean,
+        judgeTrueTask: (player: Player, message: String) -> Unit,
+        judgeFalseTask: (player: Player, message: String) -> Unit
+    ): Boolean {
+        playerTask[player.name]?.let {
+            if (it.timeStamp < System.currentTimeMillis()) {
+                playerTask.remove(player.name)
+                return false
+            }
+        }
+        playerTask[player.name] = ChatAcceptParameterTask(
+            System.currentTimeMillis() + inputTime * 1000,
+            cancelJudgeTask,
+            cancelSuccessTask,
+            judge,
+            judgeTrueTask,
+            judgeFalseTask
+        )
+        return true
+    }
+
+
+}
