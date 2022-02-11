79be91cd6cd6b2c157f8847d2268471463cf170e
WCPE <1837019522@qq.com>
2022-02-11 18:55:59 +0800

    <feat>: 更新 nukkit 的 Inventory Location 工具类

--- wcpelib-nukkit/src/main/kotlin/top/wcpe/wcpelib/nukkit/utils/InventoryUtil.kt
+++ wcpelib-nukkit/src/main/kotlin/top/wcpe/wcpelib/nukkit/utils/InventoryUtil.kt
@@ -1,6 +1,8 @@
 package top.wcpe.wcpelib.nukkit.utils
 
 import cn.nukkit.inventory.Inventory
+import cn.nukkit.item.Item
+import top.wcpe.wcpelib.nukkit.extend.getWcpeLibCustomName
 
 /**
  * 由 WCPE 在 2022/1/6 18:45 创建
@@ -23,6 +25,7 @@ object InventoryUtil {
      * @author WCPE
      * @since 1.0.8-alpha-dev-1
      */
+    @JvmStatic
     fun hasNullSlot(inventory: Inventory, sureNullSlot: Int): Boolean {
         var temp = 0
         for (i in 0 until inventory.size) {
@@ -33,6 +36,67 @@ object InventoryUtil {
             }
         }
         return temp >= sureNullSlot
+    }
+
+    /**
+     * <p>获取背包中物品数量
+     *
+     * @param inventory 玩家背包
+     * @param hasItem 判断的物品
+     * @return 物品数量
+     * @author WCPE
+     * @since 1.0.9-alpha-dev-2
+     */
+    @JvmStatic
+    fun inventoryHasItem(inventory: Inventory, hasItem: Item): Int {
+        return inventory.contents.filter {
+            it.value.id != 0 && hasItem.id == it.value.id && hasItem.damage == it.value.damage && hasItem.getWcpeLibCustomName() == it.value.getWcpeLibCustomName() && ItemUtil.equalsLore(
+                hasItem.lore.toMutableList(),
+                it.value.lore.toMutableList()
+            )
+        }.map { it.value.getCount() }.sum()
+    }
+
+    /**
+     * <p>清理背包中的指定物品
+     *
+     * @param inventory 玩家背包
+     * @param clearItem 清理物品
+     * @param clearAmount 清理数量
+     * @author WCPE
+     * @since 1.0.9-alpha-dev-2
+     */
+    @JvmStatic
+    fun clearItem(inventory: Inventory, clearItem: Item, clearAmount: Int) {
+        var tempAmount = clearAmount
+        for (i in 0 until inventory.size) {
+            val item = inventory.getItem(i)
+            if (tempAmount <= 0) {
+                return
+            }
+            if (item.id == 0) {
+                continue
+            }
+            if (clearItem.id != item.id) {
+                continue
+            }
+            if (clearItem.damage != item.damage) {
+                continue
+            }
+            if (clearItem.name != item.name) {
+                continue
+            }
+            if (!ItemUtil.equalsLore(clearItem.lore.toMutableList(), item.lore.toMutableList())) continue
+            val count = item.getCount()
+            if (tempAmount >= count) {
+                inventory.clear(i)
+                tempAmount -= count
+                continue
+            }
+            item.setCount(count - tempAmount)
+            tempAmount = 0
+            inventory.setItem(i, item)
+        }
     }
 
 }
--- wcpelib-nukkit/src/main/kotlin/top/wcpe/wcpelib/nukkit/utils/LocationUtil.kt
+++ wcpelib-nukkit/src/main/kotlin/top/wcpe/wcpelib/nukkit/utils/LocationUtil.kt
@@ -3,52 +3,60 @@ package top.wcpe.wcpelib.nukkit.utils
 import cn.nukkit.block.Block
 import cn.nukkit.level.Location
 
-class LocationUtil {
-    companion object {
+/**
+ * 由 WCPE 在 2022/1/25 17:37 创建
+ *
+ * Created by WCPE on 2022/1/25 17:37
+ *
+ * GitHub  : https://github.com/wcpe
+ * QQ      : 1837019522
+ * @author : WCPE
+ * @since  : v1.0.9-alpha-dev-2
+ */
+object LocationUtil {
 
-        fun isInRadius(
-            targetLocation: Location,
-            entityLocation: Location,
-            radius: Double
-        ): Boolean {
-            return targetLocation.distance(entityLocation) < radius
-        }
+    @JvmStatic
+    fun isInRadius(
+        targetLocation: Location,
+        entityLocation: Location,
+        radius: Double
+    ): Boolean {
+        return targetLocation.distance(entityLocation) < radius
+    }
 
+    @JvmStatic
+    fun isInAABB(needGo: Location, AA: Location, BB: Location): Boolean {
+        val xMax = AA.getX().coerceAtLeast(BB.getX()) + 1
+        val xMin = AA.getX().coerceAtMost(BB.getX())
+        val yMax = AA.getY().coerceAtLeast(BB.getY())
+        val yMin = AA.getY().coerceAtMost(BB.getY())
+        val zMax = AA.getZ().coerceAtLeast(BB.getZ()) + 1
+        val zMin = AA.getZ().coerceAtMost(BB.getZ())
+        return (needGo.getX() in xMin..xMax && needGo.getY() >= yMin && needGo.getY() <= yMax
+                && needGo.getZ() >= zMin && needGo.getZ() <= zMax)
+    }
 
-        fun isInAABB(needGo: Location, AA: Location, BB: Location): Boolean {
-            val xMax = AA.getX().coerceAtLeast(BB.getX())  + 1
-            val xMin = AA.getX().coerceAtMost(BB.getX())
-            val yMax = AA.getY().coerceAtLeast(BB.getY())
-            val yMin = AA.getY().coerceAtMost(BB.getY())
-            val zMax = AA.getZ().coerceAtLeast(BB.getZ()) + 1
-            val zMin = AA.getZ().coerceAtMost(BB.getZ())
-            return (needGo.getX() in xMin..xMax && needGo.getY() >= yMin && needGo.getY() <= yMax
-                    && needGo.getZ() >= zMin && needGo.getZ() <= zMax)
-        }
-
-
-        fun getAllLocation(a: Location, b: Location): List<Location> {
-            val maxX = a.floorX.coerceAtLeast(b.floorX)
-            val maxY = a.floorY.coerceAtLeast(b.floorY)
-            val maxZ = a.floorZ.coerceAtLeast(b.floorZ)
-            val minX = a.floorX.coerceAtMost(b.floorX)
-            val minY = a.floorY.coerceAtMost(b.floorY)
-            val minZ = a.floorZ.coerceAtMost(b.floorZ)
-            val list: MutableList<Location> = mutableListOf()
-            for (y in minY..maxY) {
-                for (z in minZ..maxZ) {
-                    for (x in minX..maxX) {
-                        list.add(Location(x.toDouble(), y.toDouble(), z.toDouble(), a.getLevel()))
-                    }
+    @JvmStatic
+    fun getAllLocation(a: Location, b: Location): List<Location> {
+        val maxX = a.floorX.coerceAtLeast(b.floorX)
+        val maxY = a.floorY.coerceAtLeast(b.floorY)
+        val maxZ = a.floorZ.coerceAtLeast(b.floorZ)
+        val minX = a.floorX.coerceAtMost(b.floorX)
+        val minY = a.floorY.coerceAtMost(b.floorY)
+        val minZ = a.floorZ.coerceAtMost(b.floorZ)
+        val list: MutableList<Location> = mutableListOf()
+        for (y in minY..maxY) {
+            for (z in minZ..maxZ) {
+                for (x in minX..maxX) {
+                    list.add(Location(x.toDouble(), y.toDouble(), z.toDouble(), a.getLevel()))
                 }
             }
-            return list
         }
+        return list
+    }
 
-
-
-        fun getAllBlock(a: Location, b: Location): List<Block> {
-            return getAllLocation(a, b).map { it.levelBlock }
-        }
+    @JvmStatic
+    fun getAllBlock(a: Location, b: Location): List<Block> {
+        return getAllLocation(a, b).map { it.levelBlock }
     }
 }
