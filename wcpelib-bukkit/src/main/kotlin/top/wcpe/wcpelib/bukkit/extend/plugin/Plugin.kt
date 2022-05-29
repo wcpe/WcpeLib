2e30ff36ba626c8f5fba33d7cf5b8cc080b42d5c
WCPE <1837019522@qq.com>
2022-05-29 20:40:42 +0800

    <feat>: 添加对 Scheduler Task 的 extend

---
+++ wcpelib-bukkit/src/main/kotlin/top/wcpe/wcpelib/bukkit/extend/plugin/Plugin.kt
@@ -0,0 +1,44 @@
+package top.wcpe.wcpelib.bukkit.extend.plugin
+
+import org.bukkit.Bukkit
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitTask
+
+/**
+ * 由 WCPE 在 2022/5/21 22:30 创建
+ *
+ * Created by WCPE on 2022/5/21 22:30
+ *
+ * GitHub  : https://github.com/wcpe
+ * QQ      : 1837019522
+ * @author : WCPE
+ * @since  : v1.0.12-alpha-dev-1
+ */
+fun JavaPlugin.runTask(runnable: Runnable, isAsynchronously: Boolean = false): BukkitTask {
+    return if (isAsynchronously) {
+        Bukkit.getScheduler().runTaskAsynchronously(this, runnable)
+    } else {
+        Bukkit.getScheduler().runTask(this, runnable)
+    }
+}
+
+fun JavaPlugin.runTaskLater(tick: Long, isAsynchronously: Boolean = false, runnable: Runnable): BukkitTask {
+    return if (isAsynchronously) {
+        Bukkit.getScheduler().runTaskLaterAsynchronously(this, runnable, tick)
+    } else {
+        Bukkit.getScheduler().runTaskLater(this, runnable, tick)
+    }
+}
+
+fun JavaPlugin.runTaskTimer(
+    startTick: Long,
+    repeatTick: Long = startTick,
+    isAsynchronously: Boolean = false,
+    runnable: Runnable
+): BukkitTask {
+    return if (isAsynchronously) {
+        Bukkit.getScheduler().runTaskTimerAsynchronously(this, runnable, startTick, repeatTick)
+    } else {
+        Bukkit.getScheduler().runTaskTimer(this, runnable, startTick, repeatTick)
+    }
+}
---
+++ wcpelib-nukkit/src/main/kotlin/top/wcpe/wcpelib/nukkit/extend/plugin/Plugin.kt
@@ -0,0 +1,37 @@
+package top.wcpe.wcpelib.nukkit.extend.plugin
+
+import cn.nukkit.Server
+import cn.nukkit.plugin.PluginBase
+import cn.nukkit.scheduler.TaskHandler
+
+/**
+ * 由 WCPE 在 2022/5/21 22:52 创建
+ *
+ * Created by WCPE on 2022/5/21 22:52
+ *
+ * GitHub  : https://github.com/wcpe
+ * QQ      : 1837019522
+ * @author : WCPE
+ * @since  : v1.0.12-alpha-dev-1
+ */
+fun PluginBase.runTask(runnable: Runnable, isAsynchronously: Boolean = false): TaskHandler {
+    return Server.getInstance().scheduler.scheduleTask(this, runnable, isAsynchronously)
+}
+
+fun PluginBase.runTaskLater(tick: Int, isAsynchronously: Boolean = false, runnable: Runnable): TaskHandler {
+    return Server.getInstance().scheduler.scheduleDelayedTask(this, runnable, tick, isAsynchronously)
+}
+
+fun PluginBase.runTaskTimer(
+    startTick: Int,
+    repeatTick: Int = startTick,
+    isAsynchronously: Boolean = false,
+    runnable: Runnable
+): TaskHandler {
+    return Server.getInstance().scheduler.scheduleDelayedRepeatingTask(
+        this, runnable,
+        startTick,
+        repeatTick,
+        isAsynchronously
+    )
+}
