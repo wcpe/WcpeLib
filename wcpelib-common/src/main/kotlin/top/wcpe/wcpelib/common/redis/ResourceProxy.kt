9aef1ea62c1b4b181b2b88946e720a4da4df5503
WCPE <1837019522@qq.com>
2023-08-19 15:14:55 +0800

    feat: ResourceProxy 添加 deleteKeysWithPattern 模糊批量删除

--- wcpelib-common/src/main/kotlin/top/wcpe/wcpelib/common/redis/ResourceProxy.kt
+++ wcpelib-common/src/main/kotlin/top/wcpe/wcpelib/common/redis/ResourceProxy.kt
@@ -1,7 +1,9 @@
 package top.wcpe.wcpelib.common.redis
 
 import redis.clients.jedis.Jedis
+import redis.clients.jedis.params.ScanParams
 import java.io.Closeable
+
 
 /**
  * 由 WCPE 在 2022/10/2 12:52 创建
@@ -61,6 +63,23 @@ class ResourceProxy(private val jedis: Jedis) : Closeable {
 
     override fun close() {
         jedis.close()
+    }
+
+    fun deleteKeysWithPattern(pattern: String) {
+        // 设置游标初始值
+        var cursor = ScanParams.SCAN_POINTER_START
+        // 设置匹配模式
+        val scanParams = ScanParams().match(pattern)
+        do {
+            // 执行模糊查询
+            val scanResult = jedis.scan(cursor, scanParams)
+            for (key in scanResult.result) {
+                // 删除匹配到的 key
+                jedis.del(key)
+            }
+            // 获取新的游标值
+            cursor = scanResult.cursor
+        } while (cursor != ScanParams.SCAN_POINTER_START)
     }
 
 }
