8eb884f8943ae90745d68ca33fd81be33661485c
东旭 宁 <3318029085@qq.com>
2025-05-16 19:00:37 +0800

    feat(bukkit):新增两个变量获取首次登录和最后登录时间戳

--- platform/bukkit/src/main/kotlin/top/wcpe/wcpelib/bukkit/WcpeLibPlaceholder.kt
+++ platform/bukkit/src/main/kotlin/top/wcpe/wcpelib/bukkit/WcpeLibPlaceholder.kt
@@ -24,15 +24,28 @@ class WcpeLibPlaceholder : PlaceholderExpansion() {
     }
 
     override fun getVersion(): String {
-        return "1.0.0"
+        return "1.0.1"
     }
 
     //%WcpeLib_server_name%
     //%WcpeLib_display_name%
+    //%WcpeLib_firstLoginTime_{playerName}% 首次登录时间
+    //%WcpeLib_lastLoginTime_{playerName}% 最后登录时间
     override fun onRequest(p: OfflinePlayer, identifier: String): String {
         when (identifier) {
             "server_name" -> return WcpeLib.getServerName()
             "display_name" -> return WcpeLib.getDisplayName()
+        }
+        val splitParams = identifier.split("_")
+        when (splitParams[0]) {
+            "firstLoginTime" -> {
+                val playerData = WcpeLib.dataManager.getPlayerDataByName(splitParams[1])
+                return playerData?.firstLoginTime.toString()
+            }
+            "lastLoginTime" -> {
+                val playerData = WcpeLib.dataManager.getPlayerDataByName(splitParams[1])
+                return playerData?.lastLoginTime.toString()
+            }
         }
         return ""
 
